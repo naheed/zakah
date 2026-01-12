5e27d6113e25b20d217d13887978044d65c175cc
naheed <naheed@gmail.com>
2026-01-11 23:42:07 -0800

    mobile testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,14 @@ export default defineConfig({
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
+        {
+            name: 'Mobile Chrome',
+            use: { ...devices['Pixel 5'] },
+        },
+        {
+            name: 'Mobile Safari',
+            use: { ...devices['iPhone 12'] },
+        },
     ],
     webServer: {
         command: 'npm run dev',
